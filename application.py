08680fa956ca1b929800af754d8cc6c45eb675f9
pkro <pkrombach@web.de>
2019-05-08 11:18:43 +0200

    changed messages data structure

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 from collections import defaultdict
 from collections import deque
+from collections import namedtuple
 
 from flask import Flask
 from flask import render_template
@@ -20,14 +21,13 @@ rooms = set('lobby')
 
 users = set('System')
 
-messages = []
-
+Message = namedtuple('Message', ['timestamp', 'user', 'message'])
 messages = defaultdict(deque)
 # Using deque of size 100, entries "older than" 100 will be purged
 # index 0 is always the latest message
 messages['lobby'] = deque([], 100)
-
-messages['lobby'].appendleft((get_timestamp(), 'System', 'Welcome to the lobby'))
+initial_message = Message(get_timestamp(), 'System', 'Welcome to the lobby')
+messages['lobby'].appendleft(initial_message)
 
 debug = os.getenv("FLASK_DEBUG") == "1"
 
